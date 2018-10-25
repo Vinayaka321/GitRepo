43181fb2304bc19c82b52f682c761de3856771e7
Vinayaka <Vinayaka@DESKTOP-2878DRR>
2018-10-25 10:45:07 +0530

    Taking connection parameters from properties file instead of hardcoding

--- employee-webapplication/src/com/saartha/dom/dao/EmployeeDAO.java
+++ employee-webapplication/src/com/saartha/dom/dao/EmployeeDAO.java
@@ -1,5 +1,7 @@
 package com.saartha.dom.dao;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -7,6 +9,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 import com.saartha.dom.application.Employee;
 import com.saartha.dom.application.Validation;
@@ -16,23 +19,38 @@ public class EmployeeDAO implements IEmployeeDAO {
 	private Connection connection;
 	private PreparedStatement statement;
 	private ResultSet resultSet;
+
 	private EmployeeDAO() {
-		String url = "jdbc:postgresql://localhost:5432/employee";
-		String userName = "postgres";
-		String password = "root";
+		Properties properties = new Properties();
+		FileInputStream in = null;
 		try {
-			Class.forName("org.postgresql.Driver");
-			this.connection = DriverManager.getConnection(url, userName, password);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			in = new FileInputStream("e:/jdbc/employee/employee.properties");
+			properties.load(in);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		} finally {
+			try {
+				if (null != in)
+					in.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		String driver = properties.getProperty("jdbc.driver");
+		String url = properties.getProperty("jdbc.url");
+		String username = properties.getProperty("jdbc.username");
+		String password = properties.getProperty("jdbc.password");
+		try {
+			Class.forName(driver);
+			connection = DriverManager.getConnection(url, username, password);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
 	public static EmployeeDAO getInstance() {
-		if(instance == null) {
+		if (instance == null) {
 			instance = new EmployeeDAO();
 		}
 		return instance;
@@ -45,19 +63,18 @@ public class EmployeeDAO implements IEmployeeDAO {
 		PreparedStatement ps = null;
 		try {
 			String queryString = "insert into employee(name,salary,age) values(?,?,?)";
-			ps= this.connection.prepareStatement(queryString);
+			ps = this.connection.prepareStatement(queryString);
 			ps.setString(1, employee.getName());
 			ps.setDouble(2, employee.getSalary());
 			ps.setInt(3, employee.getAge());
 			int insert = ps.executeUpdate();
-			if(insert!=0) {
+			if (insert != 0) {
 				inserted = true;
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			try {
 				ps.close();
 			} catch (SQLException e) {
@@ -76,10 +93,10 @@ public class EmployeeDAO implements IEmployeeDAO {
 		Employee employee = null;
 		try {
 			String queryString = "select * from employee where \"employeeId\"=?";
-			preparedStatement= this.connection.prepareStatement(queryString);
+			preparedStatement = this.connection.prepareStatement(queryString);
 			preparedStatement.setInt(1, empId);
 			resultSet = preparedStatement.executeQuery();
-			while(resultSet.next()) {
+			while (resultSet.next()) {
 				int id = resultSet.getInt("employeeId");
 				String name = resultSet.getString("name");
 				double salary = resultSet.getDouble("salary");
@@ -90,8 +107,7 @@ public class EmployeeDAO implements IEmployeeDAO {
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			try {
 				resultSet.close();
 				preparedStatement.close();
@@ -111,9 +127,9 @@ public class EmployeeDAO implements IEmployeeDAO {
 		List<Employee> employees = new ArrayList<Employee>();
 		try {
 			String queryString = "select * from employee";
-			preparedStatement= this.connection.prepareStatement(queryString);
+			preparedStatement = this.connection.prepareStatement(queryString);
 			resultSet = preparedStatement.executeQuery();
-			while(resultSet.next()) {
+			while (resultSet.next()) {
 				int id = resultSet.getInt("employeeId");
 				String name = resultSet.getString("name");
 				double salary = resultSet.getDouble("salary");
@@ -125,8 +141,7 @@ public class EmployeeDAO implements IEmployeeDAO {
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			try {
 				resultSet.close();
 				preparedStatement.close();
@@ -137,61 +152,60 @@ public class EmployeeDAO implements IEmployeeDAO {
 		}
 		return employees;
 	}
+
 	@Override
 	public boolean deleteEmployees(List<String> employeeIds) {
 		// TODO Auto-generated method stub
 		boolean isDeleted = false;
-		if(employeeIds!=null && !employeeIds.isEmpty()) {
+		if (employeeIds != null && !employeeIds.isEmpty()) {
 			PreparedStatement ps = null;
-			for(String employeeId : employeeIds)
-			{
-			try {
-				String queryString = "delete from employee where \"employeeId\" = ?";
-				ps= this.connection.prepareStatement(queryString);
-				System.out.println(Integer.parseInt(employeeId));
-				ps.setInt(1, Integer.parseInt(employeeId));
-				int deleted = ps.executeUpdate();
-				if(deleted!=0) {
-					isDeleted = true;
-				}
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			finally {
+			for (String employeeId : employeeIds) {
 				try {
-					ps.close();
+					String queryString = "delete from employee where \"employeeId\" = ?";
+					ps = this.connection.prepareStatement(queryString);
+					System.out.println(Integer.parseInt(employeeId));
+					ps.setInt(1, Integer.parseInt(employeeId));
+					int deleted = ps.executeUpdate();
+					if (deleted != 0) {
+						isDeleted = true;
+					}
 				} catch (SQLException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
+				} finally {
+					try {
+						ps.close();
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 				}
-			}
 			}
 		}
 		return isDeleted;
 	}
+
 	@Override
 	public boolean updateEmployee(Employee employee) {
 		// TODO Auto-generated method stub
 		boolean isUpdated = false;
-		if(Validation.validateEmployee(employee)) {
+		if (Validation.validateEmployee(employee)) {
 			PreparedStatement ps = null;
 			try {
 				String queryString = "update employee set name = ?,salary = ?,age = ? where \"employeeId\" = ?";
-				ps= this.connection.prepareStatement(queryString);
+				ps = this.connection.prepareStatement(queryString);
 				ps.setString(1, employee.getName());
 				ps.setDouble(2, employee.getSalary());
 				ps.setInt(3, employee.getAge());
 				ps.setInt(4, employee.getEmployeeId());
 				int update = ps.executeUpdate();
-				if(update!=0) {
+				if (update != 0) {
 					isUpdated = true;
 				}
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}
-			finally {
+			} finally {
 				try {
 					ps.close();
 				} catch (SQLException e) {
